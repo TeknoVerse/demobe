92f17453ec08875f2d593ba4b9ce4e091aef9668
gema1533 <ropuhanr@gmail.com>
2023-08-04 16:49:26 +0700

    update

--- src/models/master/TmastProduct.js
+++ src/models/master/TmastProduct.js
@@ -2,62 +2,30 @@ import { DataTypes } from "sequelize";
 import Database from "../../../config/database/Database.js";
 
 const TmastProduct = Database.define("tmast_product", {
-    assy_no : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    circuit_no : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    type_name : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    category : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    ct : {
-        type : DataTypes.FLOAT,
-        allowNull : true
-    },
-    qty_perct : {
-        type : DataTypes.INTEGER,
-        allowNull : true
-    },
-    note : {
-        type : DataTypes.STRING,
-        allowNull : true
-    }, 
-    wire_part_no : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    terminal_left : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    terminal_right : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    applicator_1 : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    applicator_2 : {
-        type : DataTypes.STRING,
-        allowNull : true
-    },
-    ct_planning : {
-        type : DataTypes.INTEGER,
-        allowNull : true
-    },
-    t_dandori : {
-        type : DataTypes.INTEGER,
-        allowNull : true
-    },
+  slock : {
+    type : DataTypes.STRING,
+    allowNull : true
+  },
+  part_no : {
+    type : DataTypes.STRING,
+    allowNull : true
+  },
+  part_name : {
+    type : DataTypes.STRING,
+    allowNull : true
+  },
+  qty : {
+    type : DataTypes.INTEGER,
+    allowNull : true
+  },
+  from : {
+    type : DataTypes.STRING,
+    allowNull : true
+  },
+  to : {
+    type : DataTypes.STRING,
+    allowNull : true
+  },
 },{
     tableName : "tmast_product"
 })
